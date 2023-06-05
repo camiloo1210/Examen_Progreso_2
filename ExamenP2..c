24ad7c694fd8544c399d01d7f352e7968d1e6e93
camiloo1210 <130024293+camiloo1210@users.noreply.github.com>
2023-06-05 16:27:06 -0500

    Impresion Bien realizada

--- ExamenP2..c
+++ ExamenP2..c
@@ -9,7 +9,8 @@ int generarNumeroAleatorio(int min, int max) {
 
 // Función para imprimir una matriz
 void imprimirMatriz(int** matriz, int filas, int columnas) {
-    for (int i = 0; i < filas; i++) {
+    for (int i = 1; i < filas; i++) {
+            printf("Alumno # %i :  ",i);
         for (int j = 0; j < columnas; j++) {
             printf("%i ", matriz[i][j]);
         }
@@ -17,6 +18,7 @@ void imprimirMatriz(int** matriz, int filas, int columnas) {
     }
     printf("\n");
 }
+ 
 
 int main() {
     srand(time(0)); // Inicializar la semilla para generar números pseudo aleatorios
@@ -27,25 +29,29 @@ int main() {
     for (int i = 0; i < numMatrices; i++) {
         matriz[i] = (int**)malloc(filas * sizeof(int*));
         for (int j = 0; j < filas; j++) {
-            printf("Alumno # %i",j);
             matriz[i][j] = (int*)malloc(columnas * sizeof(int));
             for (int k = 0; k < columnas; k++) {
                 matriz[i][j][k] = generarNumeroAleatorio(1, 10);//Aqui sabe que el valor minimo es 1 y 10 el maximo y se generan los valores a partir de la funcion generar numero aleatorio
             }
         }
     }
+
+
     // Imprimir las matrices generadas
     printf("Matrices generada con los valores aleatorios:\n");
     for (int i = 0; i < numMatrices; i++) {
+        printf("\n");
+        printf("Software    :");
+        printf("P1 ");
+        printf("P2 ");
+        printf("P3 \n");
         imprimirMatriz(matriz[i], filas, columnas);
     }
 
-    int** matrizResultado = (int**)malloc(filas * sizeof(int*));
-    for (int i = 0; i < filas; i++) {
-        matrizResultado[i] = (int*)malloc(columnas * sizeof(int));
-    }
-
+ 
     return 0;
+
+
 }
 
 
